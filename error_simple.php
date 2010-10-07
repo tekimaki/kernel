505c934fba6ecd9a5baf3f5ca875a11ebfac0a49
Will James <will@tekimaki.com>
2010-10-07 15:14:52 -0400

    get ref to gBitSystem

--- error_simple.php
+++ error_simple.php
@@ -24,6 +24,7 @@
 
 		<?php
 			if (isset($_REQUEST['error']) and !is_null($_REQUEST['error'])) {
+				global $gBitSystem;
 				if( $gBitSystem->isFeatureActive( 'site_closed' )){
 					echo $_REQUEST['error']; 
 				} else {
